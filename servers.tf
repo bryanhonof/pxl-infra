0197a579bc7494627b2b904b20e3823ce424a975
ThibeVanOrshaegen <12200712@student.pxl.be>
2023-05-31 17:28:01 +0200

    init

--- servers.tf
+++ servers.tf
@@ -38,7 +38,7 @@ locals {
       ssh_keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDasSs+aNMEYu5hR9ALtYdE5rA0vXqgFuNn0jnuXXtqg bramt@LAPTOP-VDFCIIQ9"
       ]
     }
-    teamJnM = {
+    teamJnM = { 
       ssh_keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKcPhLMq6tXObUVTKWh0jIfosPFqyAhvsgDNQs3gNQH jaspe@MSI", "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEoqsX6vl6LKlNUmeHFJc6geEhe3K0RVO+nTrRWylpT6 jonas@DESKTOP-HD1COPB"]
     }
     bryan = { ssh_keys = [] }
