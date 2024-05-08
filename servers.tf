73dfaa1e7c7dd12fd80d119e8350806fc71d8c2b
Laye19 <Laye.kone@student.pxl.be>
2024-05-08 16:15:41 +0200

    First commit in web_tech

--- servers.tf
+++ servers.tf
@@ -13,11 +13,16 @@ locals {
       ssh_keys = [
         "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMV/QrTO9+A0I2VCknmL56pEI+1Ekw6/9s6613NLPxTZAAAACHNzaDpmbG94 bryanhonof@Bryans-MBP",
       ]
-    }
+    } 
+    Laye = {
+    	ssh_key = [
+		"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCnk//AEh5iMnkfdiEvht0lAqqyzbMCQ0PxUfNUEQOcGrzIzCu9aZfFIP4hhBztEyZCHiBCD2KEv1Bg6gAzRfycgQl/fv9i6SDBwJ0h9zvNT26g90izjroO4PSUP/PrQ5v1AfVJPhGxDWlj6sI11WBTDBO8651Nl0NlIVBxT47DKPpKhMELFAP4oK4O9p0EMOBFebyCTnM9kwyhwFgqg3gtGz8mZ9+WImXOggC4kohIBQTbWw97tlzmxo86BUzjkRpr8xSa6jhWOOSaRCcmLr5B9dKcQFlZRvpxHMES0W4NnL2HssNCZPb2YST3KHnBWW75DNR3nOCWLE+D6wsFAppZfSyj5H6aDo6EywY3O96xeIGg+sknwnSlyLBJ3DRKCXN/saVduDZRrU3UrN1Psudd9USR4INTQr1caBalWGt4P9P3c2Z/vQYScuViEMaq+Bst+rIRzZprpsNHwI7tZ/d5FP6CO7Dt5fS0HMmj0IKf9bBZktzirsQRyHymXp8bH1lmWm/JaKcx5yW7a1BkghPC3vaUfxnHA9MN5Fl+fHcWwWM2Ujzq+Ua+3bvaRRc8OsSF6CTrpsNWwPM/118kvPiFSuGa6YvK6JXypW0Wr/kcQUr+EJTF7cFPG47+Qsul0KeoAyKLjnO83W5QxCxtPhyhE0zqlFkG2p/f1OLFMhzGNw== layson_sali@LaysonSali"
+		]
+	}
     Robert = {
       ssh_keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIANqWnQLgp7HX+FTgKNn3TBAPL//vO8cHGnNsqGIFArK jansenrobert4@gmail.com",
-      ]
+ 	     ]
     }
     axel = {
       ssh_keys = [
