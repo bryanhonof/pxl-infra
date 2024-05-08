e14019afdb434dea4c73f4da3316063b53d238d4
Bjarni Heselmans <12302152@student.pxl.be>
2024-05-08 15:53:42 +0200

    hallo ik ben het

--- servers.tf
+++ servers.tf
@@ -29,6 +29,11 @@ locals {
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILe8frS9W779nR/TVBTc1giVLt2bwfTYnfqMt7D2qozB 12300247@student.pxl.be",
       ]
     }
+    Bjarni = {
+      ssh_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMDu2VdroRSdGoDyVhGW626ePZaLQCwa7m1khXq4odfI 12302152@student.pxl.be",
+      ]
+    }
   }
 }
 
