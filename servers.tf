5aa058cba7bd054d514f4cbe5fc5ebbb490d20d1
Laye19 <Laye.kone@student.pxl.be>
2024-05-08 16:44:41 +0200

    added ssh_key

--- servers.tf
+++ servers.tf
@@ -34,6 +34,16 @@ locals {
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILe8frS9W779nR/TVBTc1giVLt2bwfTYnfqMt7D2qozB 12300247@student.pxl.be",
       ]
     }
+    Bjarni = {
+      ssh_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMDu2VdroRSdGoDyVhGW626ePZaLQCwa7m1khXq4odfI 12302152@student.pxl.be",
+      ]
+    }
+    Runar = {
+      ssh_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBKgm5n+eS/QiGrUHD6i2zFJRsfdEJtzhgJXLggQjuEz runar@runar"
+      ]
+    }
   }
 }
 
