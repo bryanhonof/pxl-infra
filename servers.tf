204ba020e9b461fbb951b4d1144a9006d3862c94
Devlin-Maes <devlin.maes@outlook.be>
2024-05-16 09:23:47 +0200

    Update servers.tf

--- servers.tf
+++ servers.tf
@@ -8,19 +8,68 @@ locals {
     #     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIuAcOS6oFayeYpmbe8xDPGKJels0OWq0NzrZxWCLX7d"
     #   ]
     # }
- trunk
-     Devlin = {
-        ssh_keys = [
-          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDIuvBoG+nC8PBfVbJw5Ap+3MOfU55WKBXBdUek4UGQfvF1W5jLIvdh6qwmkBh1tk97TxDJQ8lK55JvStgLBetUD3bs8JM5r1a/5gymztG0snGVZsXw4ZHxee1ROCVWqI0x739NHyC8/RBH5qCDIlOMSILLaJATMGBEceoAJK8z4tT5IdE89fNNPSyg8RxGlQfbasEYtn8+KmVsxC3vHakP2RZ4kPbIYiBDNeTOYbnOhMZ9zSa3HLmxoqUlbDwQrl+lwLM8tPPnWLLaeNZpbNXhDHNSxziHvbd2ot4WZhb69q1rfvK1W0A+wk14Y/DZPXJVty1Qgrf9ZP1Kf4qMGqd7WA270RpKxlo5zqbk7pq85XuCfoIPgnDcRSUlWXVPDFV1k9n01HtUg04uYft/bpdNNvT0F3W/+Ff3uppKoBl+CUo5wRWQeODLkq9+x+NZbeJFEbcrD3vZvAL+X51FclxquicMxZYGLPU6+gMZb4+bvbJDbPpgdIq6R57FMfyVpuk= 12300703@5CD322B20B"
-       ]
-     }
-
-    bryan = {
+	
+	bryan = {
       ssh_keys = [
         "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMV/QrTO9+A0I2VCknmL56pEI+1Ekw6/9s6613NLPxTZAAAACHNzaDpmbG94 bryanhonof@Bryans-MBP",
       ]
     }
- trunk
+    Robert = {
+      ssh_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIANqWnQLgp7HX+FTgKNn3TBAPL//vO8cHGnNsqGIFArK jansenrobert4@gmail.com",
+      ]
+    }
+    axel = {
+      ssh_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO4AjwqPqDOsot89hPrGqj8P6GXlXZP1P/JcOov1VGIX Axel.colson@student.pxl.be",
+      ]
+    }
+    quinten = {
+      ssh_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILe8frS9W779nR/TVBTc1giVLt2bwfTYnfqMt7D2qozB 12300247@student.pxl.be",
+      ]
+    }
+    Runar = {
+      ssh_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBKgm5n+eS/QiGrUHD6i2zFJRsfdEJtzhgJXLggQjuEz runar@runar"
+      ]
+    }
+    Christophe = {
+      ssh_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ/TzK2ORvoOQXWLWnQQcHoEuDY7PUlp051mwiaQpzeQ pandora@gmail.com",
+
+      ]
+    }
+    Bjarni = {
+      ssh_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMDu2VdroRSdGoDyVhGW626ePZaLQCwa7m1khXq4odfI 12302152@student.pxl.be",
+      ]
+    }
+    yinnis = {
+      ssh_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOqrPVkvGWeMpVz4a/jyUiKhkOcMi7UjVaRjoTCSTB8A 12302157@student.pxl.be",
+      ]
+    }
+    Anish = {
+      ssh_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIrvcSz2EnQieZyeUOm7aiQN+WCng1uH8fp2Nt/BrAuo Anish.Pun@student.pxl.be",
+      ]
+    }
+    Ward = {
+      ssh_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIPBikgZA3bQmkp25kk2A9XKAWbdVro1DXVmng1p0+X5 12302622@student.pxl.be",
+      ]
+    }
+	Bowen = {
+      ssh_keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAl4DIqbqunk0BYKzYY18063yFdqVAlSPhBbrZ/l24Db bowen.liang@student.plx.be",
+      ]
+    }
+	Devlin = {
+		ssh_keys = [
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINXXwwetHXIi76JQeo6H5emP0zIABhurDXyrtYLIVQn/ 12300703@student.pxl.be",
+		]
+	}
   }
 }
 
