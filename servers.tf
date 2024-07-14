f615851c6b048aefd4c2728ece3010b93b6aca63
BowenS123 <145456083+BowenS123@users.noreply.github.com>
2024-07-14 22:56:20 +0200

    Update servers.tf
    Signed-off-by: BowenS123 <145456083+BowenS123@users.noreply.github.com>

--- servers.tf
+++ servers.tf
@@ -31,7 +31,7 @@ locals {
       ssh_keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHZuoIwLhSLkChxOWJZqb+YpLMoE1ql0OHrLCh2fq14O bowen.liang@student.pxl.be"
       ]
-    }  
+    }
   }
 }
 
