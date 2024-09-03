1c450bc0c2b601e2ff3713b858a4a17f7f6636aa
Bryan Honof <bryan.honof@pxl.be>
2024-09-03 13:05:19 +0200

    chore: Remove servers

--- servers.tf
+++ servers.tf
@@ -11,21 +11,6 @@ locals {
     #     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMV/QrTO9+A0I2VCknmL56pEI+1Ekw6/9s6613NLPxTZAAAACHNzaDpmbG94 bryanhonof@Bryans-MBP"
     #   ]
     #
-    yinnis = {
-      ssh_keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOqrPVkvGWeMpVz4a/jyUiKhkOcMi7UjVaRjoTCSTB8A 12302157@student.pxl.be",
-      ]
-    }
-    quinten = {
-      ssh_keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILe8frS9W779nR/TVBTc1giVLt2bwfTYnfqMt7D2qozB 12300247@student.pxl.be"
-      ]
-    }
-    Bjarni = {
-      ssh_keys = [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDAq0D+rESyOGnAfqEjyWo+Qe4JNJp3rwBOYasEm0h6Tj3Z7yrWtF16bIApuR9l/crShtui0cf9WtzgCmhPmI4HhoIIv5nVo8ERU1E/QdG27JOGSypLusds7PgAyiRLUqufBpi7hD3C5R/xxerllNDvXFC/k62sIkPYeEkkKsUsiXKAvFrA8723O7Z3fNM1/EFZ2hhw+VKhYq0vQ8OVVZQ/VFOb1yeJJxXCzuslfPRx1I67YCTvnwuPSf93yvO+1Q/lPoWZsNf7okOLHZxjmaRAty6eXwHCtk7PiIjCjU++RdJtaQPiNzoI3S17dvAgFnCQYW09unNePfGbJQTvHLCFlKJQA9AE2uL5SEHyAp9ND5SMUK+zJEn59pDRzxX/j3SD6lx3iS1vy25NPQc6XUAuGUMg70lAQlsgAAWrUH3yRnG2CNA9m3OG9xl+SFrR4vkAgZodtspqJEUTo+rXE+0Q31+WNUfUMN69OspKmizLcJtyMNSDjfr056OP/cVIkoU= bjarni heselmans@5CD322B28R",
-      ]
-    }
     Anish = {
       ssh_keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIrvcSz2EnQieZyeUOm7aiQN+WCng1uH8fp2Nt/BrAuo Anish.Pun@student.pxl.be",
